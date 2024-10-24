7300c917d2fb2e36ce2cb206bc5722f8d2b20140
NhatLe2002 <nhatlvmse160608@fpt.edu.vn>
2024-10-24 18:31:55 +0700

    Fix user

--- SecurityGateApv.Domain/Models/Department.cs
+++ SecurityGateApv.Domain/Models/Department.cs
@@ -14,6 +14,18 @@ namespace SecurityGateApv.Domain.Models
         {
             
         }
+
+        public Department(int departmentId, string departmentName, string description, DateTime createDate, DateTime updateDate, int acceptLevel, string status)
+        {
+            DepartmentId = departmentId;
+            DepartmentName = departmentName;
+            Description = description;
+            CreateDate = createDate;
+            UpdateDate = updateDate;
+            AcceptLevel = acceptLevel;
+            Status = status;
+        }
+
         internal Department(string departmentName, string description, 
             DateTime createDate, DateTime updatedDate, int acceptLevel)
         {
@@ -31,12 +43,22 @@ namespace SecurityGateApv.Domain.Models
         public DateTime CreateDate { get; set; }
         public DateTime UpdatedDate { get; set; }
         public int AcceptLevel { get; set; }
+        public string Status { get; set; }
         public ICollection<User> User { get; set; }
+        public DateTime UpdateDate { get; }
+
         public static Result<Department> Create(string departmentName, string description,
              int acceptLevel)
         {
             var department = new Department(departmentName, description,
                 DateTime.Now, DateTime.Now, acceptLevel);
+            return department;
+        }
+        public static Result<Department> Create(int departmentId, string departmentName,DateTime createDate, DateTime updateDate, string description,
+             int acceptLevel, string status)
+        {
+            var department = new Department(departmentId, departmentName, description,
+                createDate, updateDate, acceptLevel, status);
             return department;
         }
         public Result<Department> Update() {
